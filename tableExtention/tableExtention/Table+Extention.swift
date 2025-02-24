0b651d25f91b43785eb09154f61e391119a53123
SathishKumar <sathishkumar.thulasiraman@jasmin-infotech.com>
2025-02-24 15:10:58 +0530

    Tableview List UIKIT

---
+++ tableExtention/tableExtention/Table+Extention.swift
@@ -0,0 +1,8 @@
+//
+//  Table+Extention.swift
+//  tableExtention
+//
+//  Created by JIDTP1408 on 24/02/25.
+//
+
+import Foundation
---
+++ tableExtention/tableExtention/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableExtention
+//
+//  Created by JIDTP1408 on 24/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
